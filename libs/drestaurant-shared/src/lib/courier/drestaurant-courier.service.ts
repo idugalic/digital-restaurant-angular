ce47e4b2fb546d66341cefaec3c992489d37d1f4
Ivan Dugalic <ivan.dugalic@axoniq.io>
2019-05-05 00:50:48 +0200

    Fixing bug with the wrong request data

--- libs/drestaurant-shared/src/lib/courier/drestaurant-courier.service.ts
+++ libs/drestaurant-shared/src/lib/courier/drestaurant-courier.service.ts
@@ -1,10 +1,10 @@
-import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable, Inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
-import { CouriersModel } from './couriers.model';
-import { CourierModel } from './courier.model';
+import {CouriersModel} from './couriers.model';
+import {CourierModel} from './courier.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +13,23 @@ export class DrestaurantCourierService {
   constructor(
     private http: HttpClient,
     @Inject('baseURL') private baseUrl: string
-  ) { }
+  ) {
+  }
 
-  private extractListData(res) {
+  private static extractListData(res) {
     const model: CouriersModel = new CouriersModel();
     model.page = res.page;
     model.list = res._embedded.couriers || {};
     return model;
   }
 
-  private extractSingleData(res) {
+  private static extractSingleData(res) {
     return res || {};
   }
 
   public getCouriers(): Observable<CouriersModel> {
     const url: string = this.baseUrl + '/query/couriers';
-    return this.http.get(url).map(this.extractListData);
+    return this.http.get(url).map(DrestaurantCourierService.extractListData);
   }
 
   public getCouriersByParams(
@@ -39,24 +40,23 @@ export class DrestaurantCourierService {
       this.baseUrl + '/query/couriers' + '?page=' + page + '&size=' + size;
     return this.http
       .get(url)
-      .map(this.extractListData);
+      .map(DrestaurantCourierService.extractListData);
   }
 
   public getCourier(id: string): Observable<CourierModel> {
     const url: string = this.baseUrl + '/query/couriers/' + id;
-    return this.http.get(url).map(this.extractSingleData);
+    return this.http.get(url).map(DrestaurantCourierService.extractSingleData);
   }
 
   public createCourier(courier: CourierModel): Observable<any> {
     const url: string = this.baseUrl + '/command/courier/createcommand';
-    const name: CourierName = new CourierName(courier.firstName, courier.lastName);
-    const courierRequest: CreateCourierRequest = new CreateCourierRequest(name, courier.maxNumberOfActiveOrders);
+    const courierRequest: CreateCourierRequest = new CreateCourierRequest(courier.firstName, courier.firstName, courier.maxNumberOfActiveOrders);
     return this.http.post(url, courierRequest);
   }
 }
+
 export class CreateCourierRequest {
-  constructor(public name: CourierName, public maxNumberOfActiveOrders: number) { }
+  constructor(public firstName: string, public lastName: string, public maxNumberOfActiveOrders: number) {
+  }
 }
-export class CourierName {
-  constructor(public firstName: string, public lastName: string) { }
-}
+
--- libs/drestaurant-shared/src/lib/customer/drestaurant-customer.service.ts
+++ libs/drestaurant-shared/src/lib/customer/drestaurant-customer.service.ts
@@ -1,10 +1,10 @@
-import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable, Inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
-import { CustomersModel } from './customers.model';
-import { CustomerModel } from './customer.model';
+import {CustomersModel} from './customers.model';
+import {CustomerModel} from './customer.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +13,23 @@ export class DrestaurantCustomerService {
   constructor(
     private http: HttpClient,
     @Inject('baseURL') private baseUrl: string
-  ) { }
+  ) {
+  }
 
-  private extractListData(res) {
+  private static extractListData(res) {
     const model: CustomersModel = new CustomersModel();
     model.page = res.page;
     model.list = res._embedded.customers || {};
     return model;
   }
 
-  private extractSingleData(res) {
+  private static extractSingleData(res) {
     return res || {};
   }
 
   public getCustomers(): Observable<CustomersModel> {
     const url: string = this.baseUrl + '/query/customers';
-    return this.http.get(url).map(this.extractListData);
+    return this.http.get(url).map(DrestaurantCustomerService.extractListData);
   }
 
   public getCustomersByParams(
@@ -37,26 +38,24 @@ export class DrestaurantCustomerService {
   ): Observable<CustomersModel> {
     const url: string =
       this.baseUrl + '/query/customers' + '?page=' + page + '&size=' + size;
-    return this.http.get(url).map(this.extractListData);
+    return this.http.get(url).map(DrestaurantCustomerService.extractListData);
   }
 
   public getCustomer(id: string): Observable<CustomerModel> {
     const url: string = this.baseUrl + '/query/customers/' + id;
-    return this.http.get(url).map(this.extractSingleData);
+    return this.http.get(url).map(DrestaurantCustomerService.extractSingleData);
   }
 
   public createCustomer(customer: CustomerModel): Observable<any> {
     const url: string = this.baseUrl + '/command/customer/createcommand';
-    const name: CustomerName = new CustomerName(customer.firstName, customer.lastName);
-    const customerRequest: CreateCustomerRequest = new CreateCustomerRequest(name, customer.orderLimit);
+    const customerRequest: CreateCustomerRequest = new CreateCustomerRequest(customer.firstName, customer.firstName, customer.orderLimit);
     return this.http.post(url, customerRequest);
   }
 }
 
 export class CreateCustomerRequest {
-  constructor(public name: CustomerName, public orderLimit: number) { }
+  constructor(public firstName: string, public lastName: string, public orderLimit: number) {
+  }
 }
-export class CustomerName {
-  constructor(public firstName: string, public lastName: string) { }
-}
+
 
